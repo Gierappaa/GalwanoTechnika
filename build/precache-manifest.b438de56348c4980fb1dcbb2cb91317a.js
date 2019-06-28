8e607d37bb1b0a3770ec021d5d4bdc44edacd21b
Stanislaw Wielowieyski <swielowieyski@gmail.com>
2019-06-28 21:23:04 +0200

    added build folder

---
+++ build/precache-manifest.b438de56348c4980fb1dcbb2cb91317a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa183ada19a9120734050db2a5f33d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f3f38fc2bb1daa89061",
+    "url": "/static/css/main.cb9ff8bf.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9f3f38fc2bb1daa89061",
+    "url": "/static/js/main.6beb9da7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
